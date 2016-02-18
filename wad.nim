8b69b97238d52c0b52e76077eb70d18ef1c8a0b5
Rostyslav Dzinko <rostislav.dzinko@gmail.com>
2016-02-18 15:41:43 +0200

    Added possibility to read WAD files

---
+++ wad.nim
@@ -0,0 +1,60 @@
+import math
+import sequtils
+import streams
+import strutils
+
+type
+    WadKind* {.pure.} = enum
+        IWAD = (('I'.ord.int32) or ('W'.ord.int32 shl 8) or ('A'.ord.int32 shl 16) or ('D'.ord.int32 shl 24)).int32
+        PWAD = (('P'.ord.int32) or ('W'.ord.int32 shl 8) or ('A'.ord.int32 shl 16) or ('D'.ord.int32 shl 24)).int32
+
+    WadInfo* = ref object
+        kind*  : WadKind
+        lumps* : int32
+        offset*: int32
+
+    Lump* = ref object
+        offset*: int32
+        size*: int32
+        name*: string
+
+    Wad* = ref object
+        header*: WadInfo
+        directory*: seq[Lump]
+        data*: string
+
+proc newWad*(s: Stream): Wad =
+    ## Construct WAD file from stream
+    result.new
+    result.header.new
+    result.directory = @[]
+
+    defer: s.close()
+
+    result.header.kind = WadKind(s.readInt32())
+    result.header.lumps = s.readInt32()
+    result.header.offset = s.readInt32()
+
+    result.data = s.readStr(result.header.offset - s.getPosition())
+
+
+    for _ in 0 ..< result.header.lumps:
+        let lump = new(Lump)
+        lump.offset = s.readInt32()
+        lump.size = s.readInt32()
+        lump.name = $(s.readStr(8).cstring)
+        result.directory.add(lump)
+
+proc `$`*(lump: Lump): string =
+    return "$# (offset: $#, size: $#)" % [lump.name, $lump.offset, $lump.size]
+
+proc `$`*(h: WadInfo): string =
+    ## Stringify WAD header
+    result = "Wad type: $#, lumps: $#, offset: $#" % [$h.kind, $h.lumps, $h.offset]
+
+proc `$`*(w: Wad): string =
+    result = $w.header
+
+when isMainModule:
+    let wad = newWad(newFileStream("Doom2.wad"))
+    echo wad
---
+++ wad.nimble
@@ -0,0 +1,11 @@
+# Package
+
+version       = "0.1.0"
+author        = "Rostyslav Dzinko"
+description   = "WAD resources file format reader"
+license       = "MIT"
+
+# Dependencies
+
+requires "nim >= 0.13.1"
+
